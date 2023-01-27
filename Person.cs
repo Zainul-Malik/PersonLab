4c2f07c844302a849e15a4d134bd6e769bb11482
Zainul-Malik <119996723+Zainul-Malik@users.noreply.github.com>
2023-01-27 13:06:46 -0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabA2
+{
+    internal class Person
+    {
+        //Fields
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string favoriteColor;
+        private bool isWorking;
+
+        //Properties
+        public int PersonId { get => personId; set => personId = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+        public string FavoriteColor { get => favoriteColor; set => favoriteColor = value; }
+        public bool IsWorking { get => isWorking; set => isWorking = value; }
+
+        //Constructor
+        public Person(int personId, string firstName, string lastName, int age, string favoriteColor, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.favoriteColor= favoriteColor;
+            this.isWorking = isWorking;
+        }
+
+        //Methods
+        public string DisplayPersonInfo()
+        {
+            string formatted = "";
+
+            formatted += personId + ": " + firstName + " " + lastName + "'s favorite colour is " + favoriteColor;
+
+
+            return formatted;
+        }
+
+        public string ChangeFavoriteColour()
+        {
+            favoriteColor = "White";
+
+            string newSentence = DisplayPersonInfo();
+
+            return newSentence;
+        }
+
+        public override string ToString()
+        {
+            string list = "";
+
+            list += "PersonId: " + personId + "\n";
+            list += "FirstName: " + firstName + "\n";
+            list += "LastName: " + lastName + "\n";
+            list += "FavoriteColour: " + favoriteColor + "\n";
+            list += "Age: " + age + "\n";
+            list += "IsWorking: " + isWorking; ;
+
+            return list;
+        }
+
+        public string GetAgeInTenYears()
+        {
+            int ageInTenYears = age + 10;
+
+            string newAge = firstName + " " + lastName + "'s Age in 10 years is: " + ageInTenYears;
+
+            return newAge;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabA2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1,"Ian","Brooks",30,"Red",true);
+            Person person2 = new Person(2,"Gina","James",18, "Green", false);
+            Person person3 = new Person(3, "Mike", "Briscoe", 45, "Blue", true);
+            Person person4 = new Person(4,"Mary","Beals",28,"Yellow",true);
+
+            Console.WriteLine(person2.DisplayPersonInfo());
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(person1.ChangeFavoriteColour());
+            Console.WriteLine(person4.GetAgeInTenYears());
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(person4.ToString());
+            Console.WriteLine(person3.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
